7c5cf9b7f8c0dcd961660b14fbae3b10049775be
Adrian Huang <adrianhuang0701@gmail.com>
2015-01-22 14:04:13 +0800

    hw_random: Introduce HW RNG driver for STM32F429 board
    Note: the function stm32_rng_read_number is declared globally so
          that other kernel modules can leverage the function.
    
    Signed-off-by: Adrian Huang <adrianhuang0701@gmail.com>

---
+++ arch/arm/mach-stm32/include/mach/stm32-rng.h
@@ -0,0 +1,67 @@
+/*
+ * stm32-rng.h: STM32F RNG defines.
+ *
+ * Author: Adrian Huang <adrianhuang0701@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef _STM32_RNG_H
+#define _STM32_RNG_H
+
+#include <mach/stm32.h>
+
+#define STM32_RNG_BASE (STM32_AHB2PERITH_BASE + 0x60800)
+
+/*
+ * STM32 RNG: Fields of Control Register
+ */
+#define STM32_RNG_CR_EN (1 << 2)	/* RNG Enable		*/
+#define STM32_RNG_CR_IE (1 << 3)	/* RNG Interrupt Enable	*/
+
+/*
+ * STM32 RNG: Fields of Status Register
+ */
+#define STM32_RNG_SR_DRDY (1 << 0)	/* Data Ready			*/
+#define STM32_RNG_SR_CECS (1 << 1)	/* Clock Error Current Status	*/
+#define STM32_RNG_SR_SECS (1 << 2)	/* Seed Error Current Status	*/
+#define STM32_RNG_SR_CEIS (1 << 5)	/* Clock Error Interrupt Status */
+#define STM32_RNG_SR_SEIS (1 << 6)	/* Seed Error Interrupt Status	*/
+
+#define STM32_RCC_AHB2ENR_RNG (1 << 6)		/* RNG clock enable	*/
+
+#define STM32_RNG ((volatile struct stm32_rng_regs *) STM32_RNG_BASE)
+
+/* The clock of PLL48CLK is less than 48 MHz. */
+#define STM32_RNG_PLL48CLK	48
+
+/*
+ * It requires 40 periods of the PLL48CLK clock signal between two
+ * consecutive random numbers accroding to the chapter 24
+ * "Random Number Generator" of RM0090 Reference Manual.
+ * The unit of this macro is nanosecond.
+ */
+#define STM32_RNG_DELAY_NS (40 * 1000 / STM32_RNG_PLL48CLK)
+
+struct stm32_rng_regs {
+	u32	cr;	/* Control Register */
+	u32	sr;	/* Status Register */
+	u32	dr;	/* Data Register */
+};
+
+int stm32_rng_read_number(unsigned long, u32 *);
+
+#endif
---
+++ drivers/char/hw_random/stm32-rng.c
@@ -0,0 +1,86 @@
+/*
+ * RNG driver for STM32F boards. The STM32F(s) are the series products from
+ * STMicroelectronics.
+ *
+ * Hardware driver for the STM32Fxxx Random Number Generator (RNG)
+ *
+ * Author: Adrian Huang <adrianhuang0701@gmail.com>
+ *
+ * This file is licensed under  the terms of the GNU General Public
+ * License version 2. This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/delay.h>
+#include <linux/pci.h>
+#include <linux/init.h>
+#include <linux/bitops.h>
+#include <linux/hw_random.h>
+
+#include <mach/stm32-rng.h>
+
+#define PFX	KBUILD_MODNAME ": "
+
+int stm32_rng_read_number(unsigned long reg_addr, u32 *buf)
+{
+	volatile struct stm32_rng_regs *rng_regs = (void *) reg_addr;
+	int data_len = 0;
+
+	/* Delay a period of time to get the next random number. */
+	ndelay(STM32_RNG_DELAY_NS);
+
+	if (rng_regs->sr & STM32_RNG_SR_DRDY) {
+		*buf = rng_regs->dr;
+		data_len = sizeof(u32);
+	}
+
+	return data_len;
+}
+
+static int stm32_rng_data_read(struct hwrng *rng, u32 *buffer)
+{
+	return stm32_rng_read_number(rng->priv, buffer);
+}
+
+static struct hwrng stm32_rng_ops = {
+	.name		= "stm32",
+	.data_read	= stm32_rng_data_read,
+};
+
+static int __init stm32_rng_init(void)
+{
+	int err;
+
+	/* Enable RNG clock. */
+	STM32_RCC->ahb2enr |= STM32_RCC_AHB2ENR_RNG;
+
+	/* Enable RNG. */
+	STM32_RNG->cr |= STM32_RNG_CR_EN;
+
+	stm32_rng_ops.priv = (unsigned long) STM32_RNG;
+
+	err = hwrng_register(&stm32_rng_ops);
+
+	if (err)
+		printk(KERN_ERR PFX "RNG registering failed (%d)\n", err);
+
+	printk(KERN_INFO "STM32 Hardware RNG initialized\n");
+
+	return err;
+}
+
+static void __exit stm32_rng_exit(void)
+{
+	STM32_RCC->ahb2enr &= ~STM32_RCC_AHB2ENR_RNG;
+}
+
+module_init(stm32_rng_init);
+module_exit(stm32_rng_exit);
+
+MODULE_AUTHOR("Adrian Huang <adrianhuang0701@gmail.com>");
+MODULE_DESCRIPTION("H/W Random Number Generator (RNG) driver for STM32F");
+MODULE_LICENSE("GPL");
